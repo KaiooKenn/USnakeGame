ec1fbb405280414cbc10665b0d4aa6f11be59f6f
Ahmet Eren <you@example.com>
2025-08-05 13:38:52 +0300

    basic movements

--- snake_game_refactored.py
+++ snake_game_refactored.py
@@ -29,6 +29,43 @@ class Snake:
         if direction == (0, 1):  return pg.transform.rotate(image, 180) # Down
         if direction == (0, -1): return image  # Up (no rotation)
         
+    def front_func(self):
+        # Must variables to determine next position of the snake
+        self.l_direction = self.direction[0]
+        head_x, head_y = self.body_grid[0]
+        dir_x, dir_y = self.direction[0]
+        
+        new_head = (head_x + dir_x, head_y + dir_y) # New head position out of grids
+        
+        # Game over conditions
+        if (not (0 <= new_head[0] < SCREEN_SIZE // GRID_SIZE and
+                0 <= new_head[1] < SCREEN_SIZE // GRID_SIZE) or
+                (new_head in self.body_grid)):
+            return False
+        
+        # Adding new head to the body
+        self.body_grid.insert(0, new_head)
+        self.direction.insert(0, self.direction[0])  # Copy the current direction to the new head
+        self.body_pixels.insert(0, self.body_pixels[0].copy()) # Copy the pixel position of the head
+        
+        # if snake eats food
+        #if self._is_growing:
+        #    self._is_growing = False
+        #else:
+        #    self.body_grid.pop()  # Remove the tail if not growing
+        #    self.direction.pop()  # Remove the tail direction
+        self.body_grid.pop()  # Remove the tail
+        self.direction.pop()
+        self.body_pixels.pop()
+        print(self.direction)
+        return True # if not game over
+    
+    # Animation for the snake
+    def animate(self):
+        for i in range(len(self.body_pixels)):
+            target_pixel_pos = pg.Vector2(self.body_grid[i]) * GRID_SIZE
+            self.body_pixels[i] = self.body_pixels[i].lerp(target_pixel_pos, 0.25)      
+        
     def draw(self, screen, graphics):
         # -- Draw Snake -- !!! Head must be drawn last !!!
         for i, pos in enumerate(self.body_pixels):
@@ -42,10 +79,15 @@ class Snake:
                 # Draw all body parts as body for now TODO: make a func to draw body parts because it is so complicated
                 screen.blit(self.rotate(graphics["snake_body"], self.direction[i]), graphics["snake_body"].get_rect(center=center_pos))
 
-            # Draw head
-            else:               
-                screen.blit(self.rotate(graphics["under_head"], self.direction[i]), graphics["under_head"].get_rect(center=center_pos)) # Draw under head
-                screen.blit(self.rotate(graphics["snake_head"], self.direction[i]), graphics["snake_head"].get_rect(center=center_pos)) # Draw head head
+        # Draw head
+        center_pos = self.body_pixels[0] + pg.Vector2(GRID_SIZE // 2, GRID_SIZE // 2)           
+        screen.blit(self.rotate(graphics["under_head"], self.l_direction), graphics["under_head"].get_rect(center=center_pos)) # Draw under head
+        screen.blit(self.rotate(graphics["snake_head"], self.l_direction), graphics["snake_head"].get_rect(center=center_pos)) # Draw head head
+                
+    def change_direction(self, new_direction):
+        # Prevent the snake from going in the opposite direction
+        if (new_direction[0] * -1, new_direction[1] * -1) != self.l_direction[0]:
+            self.direction[0] = new_direction
             
 
             
@@ -60,7 +102,7 @@ class Game:
         # In Game Constants
         global SCREEN_SIZE, GRID_COUNT, GRID_SIZE, HEAD_SIZE
         SCREEN_SIZE = (Info_Object.current_h)*4//5 if Info_Object.current_h < Info_Object.current_w else (Info_Object.current_h)*4//5
-        GRID_COUNT = 12
+        GRID_COUNT = 25
         GRID_SIZE = SCREEN_SIZE // GRID_COUNT
         HEAD_SIZE = GRID_SIZE + (GRID_SIZE // 5) * 2
         
@@ -69,7 +111,7 @@ class Game:
         SCREEN = pg.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
         CLOCK = pg.time.Clock()
         FPS = 60
-        FRANES_PER_MOVE = 8
+        FRANES_PER_MOVE = 7
         SCORE = 0
         
         self.running = True
@@ -103,7 +145,6 @@ class Game:
         except Exception as e:
             print(f"Error loading images: {e}")
             sys.exit(1)
-        print("Assets loaded successfully!")
         
     @staticmethod
     def load_sounds():
@@ -131,28 +172,48 @@ class Game:
         except Exception as e:
             print(f"Error loading sounds: {e}")
             sys.exit(1)
-        print("Sounds loaded successfully!")
         
         
     def draw_objects(self, *args):
         SCREEN.blit(self.graphics["background"], (0, 0))
         for obj in args:
             obj.draw(SCREEN, self.graphics)
-        
+            
+    def handle_events(self, move_time: bool, snake):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+            # Handle direction changes
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP: snake.change_direction((0, -1))
+                elif event.key == pg.K_DOWN: snake.change_direction((0, 1))
+                elif event.key == pg.K_LEFT: snake.change_direction((-1, 0))
+                elif event.key == pg.K_RIGHT: snake.change_direction((1, 0))
+                
+    # Check for the positions            
+    def front_update(self, snake):
+        if not snake.front_func():
+            self.game_over = True
+            self.running = False     
+                
     def run(self):
         self.start()
         snake = Snake()
         
+        frame_counter = 0
         while self.running:
+            move_time = frame_counter % FRANES_PER_MOVE == 0
+            self.handle_events(move_time, snake)
             
-            pg.display.flip()
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    self.running = False
+            if move_time:
+                self.front_update(snake)
                     
+            snake.animate()        
             self.draw_objects(snake)
+            pg.display.flip()
             
             CLOCK.tick(FPS)
+            frame_counter += 1
             
         print("Game Over!")
         
